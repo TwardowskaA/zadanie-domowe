cabe85f1d6a4e58e44df1b47ac52260b6eb678af
Agata <twardowska123@gmail.com>
2018-08-13 19:42:09 +0200

    zadnaie

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String x = scan.nextLine();
+        System.out.println("Podaj wynik kolejnego gracza(lub stop):");
+        while (!(x.contains("stop"))) {
+            scan.nextLine();
+        }
+        if (x.contains("stop")) {
+            System.out.println("Wybierz algortym sortowania(imie/nazwisko/punkty");
+            if (x.)
+        }
+
+
+    }
+}
